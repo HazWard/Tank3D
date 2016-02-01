bab2239cf74f5d24b3ceacf31d7f431321c705cb
Starly Solon <starlysolon@gmail.com>
2016-02-01 13:26:49 -0500

    First commit

---
+++ Tank3D/Tank3D/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Tank3D")]
+[assembly: AssemblyProduct("Tank3D")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("CLG")]
+[assembly: AssemblyCopyright("Copyright © CLG 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("1c2afda9-7112-4503-847e-e1c3de885fac")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
