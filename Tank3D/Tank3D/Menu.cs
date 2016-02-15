c63433583473c28fcb5b10bd789244c8ecc65931
MaxBenoit <benoit.maxime@hotmail.com>
2016-02-15 14:32:14 -0500

    Ajout Classe Menu

---
+++ Tank3D/Tank3D/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AtelierXNA
+{
+    class Menu
+    {
+        List<TexteCentré> ListeTextes { get; set; }
+
+        public Menu()
+        {
+
+        }
+
+        //protected override void LoadContent()
+        //{
+        //    ArrièrePlan arrièrePlan = new ArrièrePlan(jeu, "Petite Carte");
+        //}
+    }
+}
